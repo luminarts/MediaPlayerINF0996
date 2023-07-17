b8b95c8ae9a50b7eecada024c740cd4d18dcb7a0
BrenoCSenna <brenosenna90@yahoo.com.br>
2023-07-17 19:36:55 -0300

    comentarios Model

--- Model/Media.cs
+++ Model/Media.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace MediaPlayerINF0996.Model
 {
+    //A classe ObservableObject, fornecida pelo Community Toolkit MVVM, permite a notificação de alterações de propriedades.
     public class Media : ObservableObject
     {
         private string name;
@@ -10,28 +11,37 @@ namespace MediaPlayerINF0996.Model
         private string icon;
         private Uri mediaPath;
 
+        //Propriedade Name, que representa o nome da mídia.
         public string Name
         {
-            get {return name;}
-            set {SetProperty(ref name, value);}
+            get { return name; }
+            //método SetProperty para notificar os assinantes sobre a alteração e atualizar o valor da propriedade.
+            set { SetProperty(ref name, value); }
         }
 
+        //Propriedade Author, que representa o autor da mídia. Segue a mesma estrutura da propriedade Name, notificando 
+        //as alterações e atualizando o valor usando o método SetProperty.
         public string Author
         {
-            get {return author;}
-            set {SetProperty(ref author, value);}
+            get { return author; }
+            set { SetProperty(ref author, value); }
         }
 
+        //Propriedade Icon, que representa o ícone associado à mídia. Também segue a mesma linha de raciocínio que 
+        //as propriedades anteriormente citadas.
         public string Icon
         {
-            get {return icon;}
-            set {SetProperty(ref icon, value);}
+            get { return icon; }
+            set { SetProperty(ref icon, value); }
         }
 
+        //propriedade MediaPath, que representa o caminho da mídia. É do tipo Uri, o que sugere que representa o 
+        //local onde o arquivo de mídia está armazenado. O getter retorna o valor atual da propriedade e o setter 
+        //utiliza o método SetProperty para notificar as alterações e atualizar o valor.
         public Uri MediaPath
         {
-            get {return mediaPath;}
-            set {SetProperty(ref mediaPath, value);}
+            get { return mediaPath; }
+            set { SetProperty(ref mediaPath, value); }
         }
     }
 }
